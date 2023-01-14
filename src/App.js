f263b9634f2fe5d265c2707a8f9b1a2000a2c0d8
Byul Kim <caxbelle@gmail.com>
2023-01-14 15:49:57 -0800

    build

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={"/"}>
       <Navigation />
       <Routes>
         <Route path="/" exact={true} element={<Home />} />
